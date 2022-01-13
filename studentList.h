9f39712e50f9f9e8f9af7e563ded018f76a8a2df
DhruviShah126 <83241658+DhruviShah126@users.noreply.github.com>
2022-01-13 18:33:28 -0500

    Update studentList.h

--- studentList.h
+++ studentList.h
@@ -7,22 +7,14 @@ public:
    ~StudentList();            // cleanup (destructor)
 
    bool ImportFile(const char* filename);
-                              // takes in the name of a file
-                              // to open and read from
-                              // returns true if the file was 
-                              // successfully opened
+                              // returns true if the file was successfully opened
    bool CreateReportFile(const char* filename);
    void ShowList() const;     // print basic list data
-   void Sort();               // sorts the list alphabetically
-                              // by last name 
+   void Sort();               // sorts the list alphabetically by last name 
+   
 private:
-   Student **students;        // stores a pointer pointing to
-                              // an array of pointers which 
-                              // store objects of the Student 
-                              // and its derived classes
-   int size;                  // stores size of the array 
-   char inputFile [31];       // stores input file name which 
-                              // will be a max of 30 characters
-   void Grow(int increase);   // grows the array the number of times
-                              // passed in as the parameter 
+   Student **students;        // stores a pointer pointing to an array of pointers which store objects of the Student and its derived classes
+   int size;                  
+   char inputFile [31];       
+   void Grow(int increase);   
 };
